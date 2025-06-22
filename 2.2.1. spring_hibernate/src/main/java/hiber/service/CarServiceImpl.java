8810c08493397bfe678d37d2e921a2be94b4d9e5
EugeneKulagin <kulagines@gmail.com>
2025-06-22 19:41:19 +0300

    Merge remote-tracking branch 'github1/Kata' into Kata

--- 2.2.1. spring_hibernate/src/main/java/hiber/service/CarServiceImpl.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/CarServiceImpl.java
@@ -2,7 +2,6 @@ package hiber.service;
 
 import hiber.dao.CarDao;
 import hiber.model.Car;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,8 +10,12 @@ import java.util.List;
 @Service
 public class CarServiceImpl implements CarService {
 
-    @Autowired
-    private CarDao carDao;
+    private final CarDao carDao;
+
+    // Внедрение через конструктор
+    public CarServiceImpl(CarDao carDao) {
+        this.carDao = carDao;
+    }
 
     @Transactional
     @Override
